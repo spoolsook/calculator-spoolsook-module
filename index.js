eb3cceeae7cef819c71c889c15fde46422a7b8c8
spoolsook <pssarayutz@gmail.com>
2019-07-25 12:06:40 +0700

    จัดเรียง format code ให้ดูมีระเบียบ

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
     plus: (first, second) => first + second,
     subtract: (first, second) => first - second,
     multiply: (first, second) => first * second,
-    divide:(first, second) => second>0 ? first/second : 'divide by zero.'
+    divide: (first, second) => second > 0 ? first / second : 'divide by zero.'
 }
