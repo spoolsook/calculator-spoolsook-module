9fdb3bdcffad4d712dec0f014ce3ff05f327ff32
spoolsook <pssarayutz@gmail.com>
2019-07-25 10:33:10 +0700

    เพิ่ม function multiply +test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
     plus: (first, second) => first + second,
-    subtract: (first, second) => first - second
+    subtract: (first, second) => first - second,
+    multiply: (first, second) => first * second
 }
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 const cal = require('./index');
 console.log(cal.plus(3, 6));
 console.log(cal.subtract(3, 6));
-
+console.log(cal.multiply(3, 6));
