66f13145ede8b7f96ef31754a6a5866893771948
spoolsook <pssarayutz@gmail.com>
2019-07-25 11:04:00 +0700

    add divide for cal

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     plus: (first, second) => first + second,
     subtract: (first, second) => first - second,
-    multiply: (first, second) => first * second
+    multiply: (first, second) => first * second,
+    divide:(first, second) => second>0 ? first/second : 0
 }
--- test.js
+++ test.js
@@ -2,3 +2,4 @@ const cal = require('./index');
 console.log(cal.plus(3, 6));
 console.log(cal.subtract(3, 6));
 console.log(cal.multiply(3, 6));
+console.log(cal.divide(6,3));
